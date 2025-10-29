d69bc9cde603b388d7c7e4408077c014fd915a3c
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-10-29 21:29:22 +0100

    feat: add Home component

--- src/Home/components/Home.tsx
+++ src/Home/components/Home.tsx
@@ -1,13 +1,22 @@
+import { Link } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 
 export default function Home() {
   const { t } = useTranslation();
 
   return (
-    <div className="text-center">
-      <h1>{t("title")}</h1>
-      <span>{t("description.part1")}</span>
-      <span>{t("description.part2")}</span>
-    </div>
+    <main className="h-full flex flex-col justify-evenly items-center p-2">
+      <h1 className="text-5xl tracking-wider">{t("home.title")}</h1>
+      <section className="flex flex-col self-start">
+        <h2 className="text-3xl">{t("home.subtitle")}</h2>
+        <p className="w-prose">{t("home.description")}</p>
+      </section>
+      <Link
+        to="/"
+        className="rounded-lg p-2 text-slate-100 bg-sky-500 hover:bg-sky-600 hover:text-slate-50 dark:bg-sky-700 dark:hover:bg-sky-600 dark:hover:text-slate-200"
+      >
+        {t("home.btn-new")}
+      </Link>
+    </main>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,13 @@ import { createFileRoute } from "@tanstack/react-router";
 import Home from "@/Home/components/Home";
 
 export const Route = createFileRoute("/")({
-  component: App,
+  component: Index,
 });
 
-function App() {
-  return <Home />;
+function Index() {
+  return (
+    <div className="h-full bg-neutral-300 dark:bg-gray-800 text-slate-800 dark:text-slate-100">
+      <Home />
+    </div>
+  );
 }
